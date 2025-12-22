af75dea6e1dfdb6d13eaeaa4d4103768195453a3
sseooh <sseooh@gmail.com>
2025-12-22 14:26:14 +0900

    feat: add search page

---
+++ src/components/icons/Building.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg width="15" height="15" viewBox="0 0 15 15" fill="none">
+    <rect
+      x="2"
+      y="3"
+      width="6"
+      height="10"
+      rx="1"
+      stroke="#A3978F"
+      stroke-width="1.2"
+      stroke-linejoin="round"
+      fill="none"
+    />
+    <circle cx="4" cy="5.5" r="0.5" fill="#A3978F" />
+    <circle cx="6" cy="5.5" r="0.5" fill="#A3978F" />
+    <circle cx="4" cy="8" r="0.5" fill="#A3978F" />
+    <circle cx="6" cy="8" r="0.5" fill="#A3978F" />
+    <rect x="4" y="10.5" width="2" height="2.5" rx="0.5" fill="#A3978F" />
+    <rect
+      x="8.5"
+      y="7"
+      width="4.5"
+      height="6"
+      rx="1"
+      stroke="#A3978F"
+      stroke-width="1.2"
+      stroke-linejoin="round"
+      fill="none"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Building",
+};
+</script>
---
+++ src/components/icons/ChevronLeft.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <path
+      d="M12.5 15L7.5 10L12.5 5"
+      stroke="#6F462E"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ChevronLeft",
+};
+</script>
