80a230c44b81c3dc164100b381872709f2e83d5b
Y-eseong <yodeng94@naver.com>
2025-12-23 11:07:11 +0900

    feat: add favorite property

--- src/stores/ui.js
+++ src/stores/ui.js
@@ -2,9 +2,13 @@ import { defineStore } from "pinia";
 
 export const useUIStore = defineStore("ui", {
   state: () => ({
+    searchMode: "ALL",
     showFavoriteCalendar: false,
   }),
   actions: {
+    setSearchMode(mode) {
+      this.searchMode = mode;
+    },
     toggleFavoriteCalendar() {
       this.showFavoriteCalendar = !this.showFavoriteCalendar;
     },
